790de4a4d8c1cc3ca536e20009937eedcf22a876
Vinizaun <88088847+Vinizaun@users.noreply.github.com>
2024-06-11 12:33:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAltermativa = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
