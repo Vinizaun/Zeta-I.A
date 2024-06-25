0f0c84a5db9b5afe8a4f0c56afe222638b0bf708
Vinizaun <88088847+Vinizaun@users.noreply.github.com>
2024-06-25 09:33:39 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPergunta = document.querySelector(".caixa-pergunta");
-const caixaAltermativa = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
