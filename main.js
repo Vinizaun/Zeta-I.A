9ec8f1d3ac57369412aa77c8da1d6f5f7cd9db2b
Vinizaun <88088847+Vinizaun@users.noreply.github.com>
2024-07-02 12:32:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado = "A reciclagem de biodegradáveis pode ser integrada ao transporte público?";
+        alternativa: [
+            {
+                texto: "Sim",
+                afirmação: "afirmação"
+            },
+            {
+                texto: "Não",
+                afirmação:"afirmação"
+            }
+        ]
+    },
+    {
+        enunciado:""
+    }
+]
