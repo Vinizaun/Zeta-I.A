253ca506d0b3e08cf27b44f0dd391a172e332be5
Vinizaun <88088847+Vinizaun@users.noreply.github.com>
2024-09-24 11:38:44 -0300

    Update perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,44 @@
+export const perguntas = [
+
+{
+    enunciado: "A reciclagem de biodegradáveis pode ser integrada ao transporte público?",
+    alternativas: [
+        {
+            texto: "Sim!",
+            afirmacao: "Porque pode diminuir a quantidade de roupas no lixo."
+        },
+        {
+            texto: "Não!",
+            afirmacao: "Porque uma coisa não tem nada a ver com a outra."
+        }
+    ],
+    proxima: 1,
+},
+{
+    enunciado: "Deve-se colocar lixeiras para resíduos biodegradáveis em estações de transporte público?",
+    alternativas: [
+        {
+            texto: "Sim!",
+            afirmacao: "Porque facilita a separação de resíduos."
+        },
+        {
+            texto: "Não!",
+            afirmacao: "Porque pode ser mal utilizado pelos usuários."
+        }
+    ],
+    proxima: 2,
+},
+{
+    enunciado: "O aumento do respeito às regras de trânsito e a redução do uso de automóveis podem contribuir para a sustentabilidade urbana?",
+    alternativas: [
+        {
+            texto: "Sim!",
+            afirmacao: "O aumento do respeito às regras de trânsito e a redução do uso de automóveis contribuem significativamente para a sustentabilidade urbana."
+        },
+        {
+            texto: "Não!",
+            afirmacao: "Mesmo com maior respeito às regras de trânsito, a redução do uso de automóveis não impactará significativamente a sustentabilidade urbana."
+        }
+    ]
+},
+];
